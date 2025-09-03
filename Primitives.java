0c37e11d798098c114bb974c6febe7e5b0a2705f
Ujjaval Goyal <ujjavalgoyal5@gmail.com>
2025-09-03 21:50:14 +0530

    ujjaval

---
+++ Primitives.java
@@ -0,0 +1,12 @@
+public class Primitives{
+    public static void main(String[] args) {
+        // int roll_no=64;
+        // String is not permitive
+        // char letter='r';
+        // float marks=43.56f;
+        // double d=45568.65;
+        // long L=1234566788L;
+        // boolean b=true;
+         
+    }
+}
