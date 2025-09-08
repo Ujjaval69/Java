f4da196ceebfeba513a1aad28636291f5c915e92
Ujjaval Goyal <ujjavalgoyal5@gmail.com>
2025-09-08 09:48:51 +0530

    ujjaval

---
+++ Switch.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Switch{
+    public static void main(String[] args) {
+         Scanner input=new Scanner(System.in);
+         String fruit=input.next();
+          
+
+    }
+}
